51a112532f4123912b0e6d86ba41c9b591cb54ba
Steven Julius <stevenjulius2003@gmail.com>
2024-03-26 23:13:36 +0700

    update for models

---
+++ app/Models/early_warning.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class early_warning extends Model
+{
+    use HasFactory;
+}
